b06eb0855e69b7d85dfc7124951f5044baed10e5
jernej kozelj <kozeljernej@gmail.com>
2016-06-23 11:09:02 +0200

    Initial commit

---
+++ app/src/main/java/com/diploma/jk/diploma/MainActivity.java
@@ -0,0 +1,86 @@
+package com.diploma.jk.diploma;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    Sensor accelerometer;
+    SensorManager sm;
+    TextView acceleration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        //ACCELEROMETER TEST
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        acceleration = (TextView)findViewById(R.id.acceleration);
+
+    }
+
+    //DODANA METODA PRI TESTIRANJU ACCELEROMETRA
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        acceleration.setText("X: "+sensorEvent.values[0]+
+                            "\nY: "+sensorEvent.values[1]+
+                            "\nZ: "+sensorEvent.values[2]);
+    }
+
+
+    //DODANA METODA PRI TESTIRANJU ACCELEROMETRA
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
